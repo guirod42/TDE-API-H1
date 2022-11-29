370764b6d4688ce0679b1d683ad9cedf5b32d1ef
Guilherme Silva Rodrigues <gui.silva.rodrigues@outlook.com.br>
2022-11-29 17:33:52 -0300

    Ajuste controllers - route

--- Aula01/Controllers/ProdutoController.cs
+++ Aula01/Controllers/ProdutoController.cs
@@ -97,7 +97,7 @@ namespace Aula01.Controllers
         }
 
         //[Authorize]
-        [Route("Obter por ID")]
+        [Route("ObterporID")]
         [HttpGet]
 		public IActionResult ObterPorId(int id)
 		{
@@ -113,7 +113,7 @@ namespace Aula01.Controllers
 		}
 
         //[Authorize]
-        [Route("Obter Todos")]
+        [Route("ObterTodos")]
 		[HttpGet]
 		public IActionResult ObterTodos()
 		{
